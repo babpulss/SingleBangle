5b9009f554b64e0e5a8295854025b2c638254bf7
pildong <babpulss@naver.com>
2020-01-12 23:31:23 +0900

    pildong - log4j added

--- src/main/java/recoder/single/bangle/AOP/Adviser.java
+++ src/main/java/recoder/single/bangle/AOP/Adviser.java
@@ -1,17 +1,23 @@
 package recoder.single.bangle.AOP;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class Adviser {
+	private static final Logger logger = LoggerFactory.getLogger(Adviser.class);
 
 	@Autowired
 	private HttpServletRequest req;
@@ -21,8 +27,10 @@ public class Adviser {
 	
 	@Around("execution(* recoder.single.bangle.index.*.*(..))")
 	public Object getInfoFromIndex(ProceedingJoinPoint pjp) {
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyy/MM/dd hh:mm:ss");
 		String ip = req.getRemoteAddr();
-		System.out.println(ip + " 에서 입장");
+		logger.debug(ip + " : " + sdf.format(d.getTime()));
 		try {
 			return pjp.proceed();
 		} catch(Throwable e) {
