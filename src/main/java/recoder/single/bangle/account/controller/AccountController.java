f4986ba215cf0e015e91077cb2b8f8ba762dc455
shin <shin>
2020-01-16 09:39:33 +0900

    shin

--- src/main/java/recoder/single/bangle/account/controller/AccountController.java
+++ src/main/java/recoder/single/bangle/account/controller/AccountController.java
@@ -1,25 +1,52 @@
 package recoder.single.bangle.account.controller;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
+import java.io.FileInputStream;
+import java.io.StringReader;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.List;
-import java.util.Map;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.css.CssFile;
+import com.itextpdf.tool.xml.css.StyleAttrCSSResolver;
+import com.itextpdf.tool.xml.html.CssAppliers;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.parser.XMLParser;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
+import com.itextpdf.tool.xml.pipeline.end.PdfWriterPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
 
 import recoder.single.bangle.account.DTO.AccountDTO;
-import recoder.single.bangle.account.component.AccountComponent;
 import recoder.single.bangle.account.service.AccountService;
+import recoder.single.bangle.account.service.PdfService;
 
 @Controller
 public class AccountController {
 	@Autowired
 	HttpSession session;
+	
+	@Inject
+	PdfService pdfService;
 	
 	@Autowired
 	HttpServletRequest request;
@@ -27,8 +54,7 @@ public class AccountController {
 	@Autowired
 	AccountService accService;
 	
-	@Autowired
-	AccountComponent component;
+
 	
 	@RequestMapping("/account")
 	public String accountIndex() {
@@ -57,23 +83,25 @@ public class AccountController {
 	@RequestMapping("/detailAccount")
 	public String detailAccount(HttpServletRequest req) {
 		String formedDate = (String)req.getParameter("formedReportingDate");
-		int incomeSUM =0;
-		int expenseSUM =0;
+		session.setAttribute("formedDate", formedDate);
+		int cashSum = 0;
+		int cardSum = 0;
+		int in = 0;
+		int out = 0;
+		String name;
 		try {
 			List<AccountDTO> list = accService.ListAllByFormedReportingDate(session,formedDate);
-			Map<String, Integer> sum = component.summary(list);
-			for(AccountDTO tmp : list) {
-				incomeSUM += tmp.getIncome();
-				expenseSUM += tmp.getExpense();
-			}
-			Date mon = list.get(0).getReportingDate();
-			SimpleDateFormat months = new SimpleDateFormat("MM");
-			String month = months.format(mon);
-			request.setAttribute("month", month);
-			request.setAttribute("incomeSUM", incomeSUM);
-			System.out.println(incomeSUM);
-			System.out.println(expenseSUM);
-			request.setAttribute("expenseSUM", expenseSUM);
+			cashSum = accService.cashSummary(list);
+			cardSum = accService.cardSummary(list);
+			in = accService.incomeSummary(list);
+			out = accService.outcomeSummary(list);
+			name = accService.getUserName(list);
+			request.setAttribute("name", name);
+			request.setAttribute("cashSum", cashSum);
+			request.setAttribute("cardSum", cardSum);
+			request.setAttribute("in", in);
+			request.setAttribute("out", out);
+			request.setAttribute("formedDate", formedDate);
 			request.setAttribute("list", list);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -81,6 +109,73 @@ public class AccountController {
 		}		
 		
 		return "accountBook/detailAccountBook";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/Account.add")
+	public String insertAccount(AccountDTO dto) {
+		String id = (String)session.getAttribute("id");
+		String userName = (String)session.getAttribute("userName");		
+		int price = Integer.parseInt(request.getParameter("price"));
+		AccountDTO dtos = new AccountDTO();
+		System.out.println(dto.getSpec());
+		if(dto.getSpec().equals("수입")) {
+			dto.setIncome(price);
+			dtos = new AccountDTO(0, id, userName, dto.getReportingDate(), null, dto.getDetails(), dto.getPayments(), dto.getSpec(), dto.getIncome(), 0, dto.getRemarks());
+			System.out.println(dtos);		 
+		}else {
+			dto.setExpense(price);
+			dtos = new AccountDTO(0, id, userName, dto.getReportingDate(), null, dto.getDetails(), dto.getPayments(), dto.getSpec(), 0, dto.getExpense(), dto.getRemarks());
+			System.out.println(dtos);
+		}
+		int result = accService.insertAccountData(dtos);
+		if(result > 0) {
+			try {
+				List<AccountDTO> list = accService.monthListById(session);
+				return new Gson().toJson(list);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	@RequestMapping("/ListViewForPDF")
+	public String listViewForPDF() {
+		int cashSum = 0;
+		int cardSum = 0;
+		int in = 0;
+		int out = 0;
+		String formedDate = (String)session.getAttribute("formedDate");
+		String name;
+		try {
+			List<AccountDTO> list = accService.ListAllByFormedReportingDate(session,formedDate);
+			cashSum = accService.cashSummary(list);
+			cardSum = accService.cardSummary(list);
+			in = accService.incomeSummary(list);
+			out = accService.outcomeSummary(list);
+			name = accService.getUserName(list);
+			request.setAttribute("name", name);
+			request.setAttribute("cashSum", cashSum);
+			request.setAttribute("cardSum", cardSum);
+			request.setAttribute("in", in);
+			request.setAttribute("out", out);
+			request.setAttribute("formedDate", formedDate);
+			request.setAttribute("list", list);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		return "accountBook/pdfAccountView";
+	}
+	
+	@RequestMapping("/accountPDF")
+	public void accountPDFprint(HttpServletRequest req,HttpServletResponse resp,String pdfValue) {
+		
+		pdfService.createPdf(req, resp, pdfValue);
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/views/accountBook/pdfAccountView.jsp
@@ -0,0 +1,157 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+<script
+	src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet"
+	href="/resources/css/accountCSS/detailAccountCSS.css" />
+<script src="/resources/js/accountJS/accountJS.js"></script>
+</head>
+<body>
+	<form id="pdf" action="${pageContext.request.contextPath }/accountPDF"
+		method="post">
+		<input type="hidden" class="pdfValue btn btn-info" id="pdfValue"
+			name="pdfValue" value="" />
+	</form>
+	<div class="container">
+		<div class="invoice row">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body p-0">
+						<div class="row p-5" style="border-bottom: 1px solid gray">
+							<div class="col-md-6" >
+								<h1 class="h1" style="font-size: 18pt">
+									<c:out value="${formedDate }" /></h1>
+									수입/지출 정보
+								
+							</div>
+
+							<div class="col-md-6 text-right">
+								<button id="printInvoice" class="btn btn-info">
+									<i class="fa fa-print"></i> Print
+								</button>
+								<button type="button" class="pdfBtn btn btn-info"
+									value="PDF DownLoad">PDF DownLoad</button>
+
+
+							</div>
+						</div>
+<!-- 						<hr class="my-1" /> -->
+						<div class="row pb-5 p-5" style="padding-bottom: 20px">
+							<div class="col-md-6">
+								<p class="font-weight-bold mb-4"></p>
+								<p class="mb-1"></p>
+
+							</div>
+
+							<div class="col-md-6 text-right" style="text-align: right">
+								<p class="font-weight-bold mb-4" style="font-weight: bold;">Payment Details</p>
+								<p class="mb-1">
+									<span class="text-muted">카드 : </span>
+									<fmt:formatNumber value="${cardSum}" pattern="###,###원" />
+								</p>
+								<p class="mb-1">
+									<span class="text-muted">현금 : </span>
+									<fmt:formatNumber value="${cashSum}" pattern="###,###원" />
+								</p>
+								<p class="mb-1">
+									<span class="text-muted">이름 : </span>
+									<c:out value="${name }" />
+								</p>
+							</div>
+						</div>
+
+						<div class="row p-5">
+							<div class="col-md-12">
+								<table class="table">
+									<thead>
+										<tr>
+											<th class="border-0 text-uppercase small font-weight-bold">날짜</th>
+											<th class="border-0 text-uppercase small font-weight-bold">용도</th>
+											<th class="border-0 text-uppercase small font-weight-bold">결제방식</th>
+											<th class="border-0 text-uppercase small font-weight-bold">입/출금</th>
+											<th class="border-0 text-uppercase small font-weight-bold">금액</th>
+											<th class="border-0 text-uppercase small font-weight-bold">비고</th>
+										</tr>
+									</thead>
+									<tbody>
+										<c:forEach items="${list }" var="list">
+											<tr>
+												<td>${list.reportingDate }</td>
+												<td>${list.details}</td>
+												<td>${list.payments }</td>
+												<td>${list.spec }</td>
+												<c:choose>
+
+													<c:when test="${list.income != 0 }">
+														<td style="color: dodgerblue"><fmt:formatNumber
+																value="${list.income}" pattern="###,###원" /></td>
+													</c:when>
+													<c:otherwise>
+														<td style="color: red"><fmt:formatNumber
+																value="${list.expense}" pattern="###,###원" /></td>
+													</c:otherwise>
+												</c:choose>
+												<td>${list.remarks}</td>
+
+											</tr>
+
+										</c:forEach>
+									</tbody>
+								</table>
+							</div>
+						</div>
+
+						<div class="d-flex flex-row-reverse bg-dark text-white p-4" style="background-color: #424242; padding-top:20px; padding-bottom:20px;">
+							<div class="py-3 px-5 text-right" style="float:left; width:35%; padding-left: 40px;">
+								<div class="mb-2" style="color:white;">총 수입 비용</div>
+								
+								<div class="h2 font-weight-light" style="color: dodgerblue">
+									<fmt:formatNumber value="${in}" pattern="###,###원" />
+								</div>
+							</div>
+
+							<div class="py-3 px-5 text-right" style="width:35%; float:left;  padding-left:30px;">
+								<div class="mb-2" style="color:white;">총 지출 비용</div>
+								<div class="h2 font-weight-light" style="color: red">
+									-
+									<fmt:formatNumber value="${out}" pattern="###,###원" />
+								</div>
+							</div>
+
+							
+							<div class="py-3 px-5 text-right" style="float:right; width:80%; padding-left:20px;">
+								<div class="mb-2" style="color:white;">총 수입/지출비용</div>
+								<div class="h2 font-weight-light"style="color:white;">
+									<fmt:formatNumber value="${in-out}" pattern="###,###원" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="text-light mt-5 mb-5 text-center small" style="text-align: center">
+			by : <a class="text-light" target=""
+				href="${pageContext.request.contextPath}/">Single Bangle</a>
+		</div>
+	</div>
+	<script>
+	$(".pdfBtn").on("click", function() {
+		$("#pdfValue").val($(".container").html());
+		console.log($("#pdfValue").val());
+		$("#pdf").submit();
+	});
+	</script>
+</body>
+</html>
