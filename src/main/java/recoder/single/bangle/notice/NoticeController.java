c400f91542a882865d2b1ee4b5f939ccab808938
pildong <babpulss@naver.com>
2020-01-24 23:25:30 +0900

    pildong

--- src/main/java/recoder/single/bangle/notice/NoticeController.java
+++ src/main/java/recoder/single/bangle/notice/NoticeController.java
@@ -19,20 +19,20 @@ public class NoticeController {
 	public String toNotice(Model m) {
 		List<NoticeDTO> list = dao.toNotice();
 		m.addAttribute("list", list);
-		return "notice";
+		return "notice/notice";
 	}
 
 	@RequestMapping("/readNotice")
 	public String update(int seq, Model m) {
 		NoticeDTO dto = dao.update(seq);
 		m.addAttribute("dto", dto);
-		return "notice";
+		return "notice/detailNoticeView";
 	}
 
 	@RequestMapping("/postNotice")
 	public String postNotice(NoticeDTO dto) {
 		dao.postNotice(dto);
-		return "notice";
+		return "notice/notice";
 	}
 
 	@RequestMapping("/updateNotice")
@@ -44,6 +44,6 @@ public class NoticeController {
 	@RequestMapping("/deleteNotice")
 	public String deleteNotice(NoticeDTO dto) {
 		dao.deleteNotice(dto);
-		return "notice";
+		return "notice/notice";
 	}
 }
