08fea7d9675ad015583d65c8ad261199aca9a7d8
pildong <babpulss@naver.com>
2020-01-27 15:06:00 +0900

    pildong

--- src/main/java/utils/EncryptionUtils.java
+++ src/main/java/utils/EncryptionUtils.java
@@ -6,15 +6,15 @@ import java.security.MessageDigest;
 public class EncryptionUtils {
 	public static String encrypt(String input) {
 
-//		String result = "";
-//		try {
-//			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-//			digest.reset();
-//			digest.update(input.getBytes("utf8"));
-//			result = String.format("%064x", new BigInteger(1, digest.digest()));
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		return input;
+		String result = "";
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			digest.reset();
+			digest.update(input.getBytes("utf8"));
+			result = String.format("%064x", new BigInteger(1, digest.digest()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
