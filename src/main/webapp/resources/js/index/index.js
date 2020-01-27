29c1bb1885d91b8681006066fc27a3ebc5263236
shin <shin>
2020-01-27 16:10:10 +0900

    shinikho

--- src/main/webapp/resources/js/index/index.js
+++ src/main/webapp/resources/js/index/index.js
@@ -76,42 +76,120 @@ $(document).ready(function() {
 	})
 	
 	
+//	var typingIdx=0; 
+//	var liIndex = 0;
+//	var liLength = $("#typing-txt>ul>li").length;
+//
+//	// 타이핑될 텍스트를 가져온다 
+//	var typingTxt = $("#typing-txt>ul>li").eq(liIndex).text(); 
+//	//liIndex 인덱스로 구분해 한줄씩 가져옴
+//
+//	typingTxt=typingTxt.split(""); // 한글자씩 잘라 배열로만든다
+//
+//	var tyInt = setInterval(typing,300); // 반복동작 
+//	     
+//	function typing(){ 
+//	  $(".typing ul li").removeClass("on");
+//	  $(".typing ul li").eq(liIndex).addClass("on");
+//	  //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
+//	  
+//	  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+//	     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+//	     typingIdx++; 
+//	   } else{ //한문장이끝나면
+//	     if(liIndex<liLength-1){
+//	     //다음문장으로  가기위해 인덱스를 1증가
+//	       liIndex++; 
+//	     //다음문장을 타이핑하기위한 셋팅
+//	        typingIdx=0;
+//	        typingTxt = $("#typing-txt>ul>li").eq(liIndex).text(); 
+//	       
+//	     //다음문장 타이핑전 1초 쉰다
+//	         clearInterval(tyInt);
+//	          //타이핑종료
+//	     
+//	            tyInt = setInterval(typing,300);
+//	          
+//	        }
+//	    } 
+//	}  
+	var typingBool = false; 
+	var typingBool1 =false;
 	var typingIdx=0; 
 	var liIndex = 0;
-	var liLength = $("#typing-txt>ul>li").length;
+	var liIndex2 = 0;
+	var liLength = $(".typing-txt1>ul>li").length;
+	var liLength2 = $(".typing-txt2>ul>li").length;
+	var del = -1;
+	var repeatInt= null;
+	var tyInt = null;
 
 	// 타이핑될 텍스트를 가져온다 
-	var typingTxt = $("#typing-txt>ul>li").eq(liIndex).text(); 
-	//liIndex 인덱스로 구분해 한줄씩 가져옴
+	var typingTxt = $(".typing-txt1>ul>li").eq(liIndex).text(); 
+	typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
 
-	typingTxt=typingTxt.split(""); // 한글자씩 잘라 배열로만든다
-
-	var tyInt = setInterval(typing,300); // 반복동작 
+	if(typingBool==false){ 
+	  // 타이핑이 진행되지 않았다면 
+	    typingBool=true; 
+	    tyInt = setInterval(typing,200); // 첫번재 반복동작 
+	} 
 	     
 	function typing(){ 
-	  $(".typing ul li").removeClass("on");
-	  $(".typing ul li").eq(liIndex).addClass("on");
-	  //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
-	  
-	  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-	     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-	     typingIdx++; 
-	   } else{ //한문장이끝나면
-	     if(liIndex<liLength-1){
-	     //다음문장으로  가기위해 인덱스를 1증가
-	       liIndex++; 
-	     //다음문장을 타이핑하기위한 셋팅
-	        typingIdx=0;
-	        typingTxt = $("#typing-txt>ul>li").eq(liIndex).text(); 
-	       
-	     //다음문장 타이핑전 1초 쉰다
-	         clearInterval(tyInt);
-	          //타이핑종료
-	     
-	            tyInt = setInterval(typing,300);
+	  if(typingIdx<typingTxt.length){ 
+	   
+	        // 타이핑될 텍스트 길이만큼 반복 
+	    $(".typing>ul>li").removeClass("on")
+	    $(".typing ul li").eq(liIndex).addClass("on")
+	    $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); 
+	        // 한글자씩 이어준다. 
+	        typingIdx++; 
+	    if(typingIdx == typingTxt.length){
+	        if(liIndex==2){
+	          clearInterval(tyInt) ;
+	          setTimeout(function(){
+	           $(".typing>ul>li").removeClass("on")
+	          },500);
+	        } else{
+	          //첫번째 단어가 써지면 1분쉰다.
 	          
-	        }
+	          clearInterval(tyInt);
+	           setTimeout(function(){
+	             tyInt = setInterval(typing,200);
+	           },200);
+	       }
+	     }
+	   } else{ 
+	     //한문장이끝나면
+	     if(liIndex==1 && typingBool1==false){
+	       if(-typingTxt.length-1 < del ){
+	         //한글자씩 지운다.
+	          $(".typing ul li").eq(liIndex).html(typingTxt.slice(0, del))
+	          del--;
+	       }else{
+	         
+	         //변수초기화 
+	         typingIdx=0;
+	         del= -1;
+	         typingTxt = $(".typing-txt2>ul>li").eq(liIndex2).text(); 
+	         liIndex2++;
+	         if(liIndex2 == liLength2 ){
+	           typingBool1=true;
+	         }
+	         
+	         //1분후 다음분장 타이핑 
+	         clearInterval(tyInt);
+	         setTimeout(function(){
+	           tyInt = setInterval(typing,200);
+	         },200);
+	       }
+	     }else{
+	        typingIdx=0;
+	        if(liIndex <= liLength-1){
+	           liIndex++;
+	         }
+	         typingTxt = $(".typing-txt1>ul>li").eq(liIndex).text(); 
+	       
+	     }
 	    } 
 	}  
-	
 });
